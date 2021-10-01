77363c40ca66bf6ee4f9a807c41ebcf169c275e5
mazuluagar56168 <mazuluagar@gmail.com>
2021-10-01 07:04:48 -0500

    CRUD Universidad

--- SICAU/SICAU.App/SICAU.App.Dominio/Entidades/Universidad.cs
+++ SICAU/SICAU.App/SICAU.App.Dominio/Entidades/Universidad.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace SICAU.App.Dominio
 {
     public class Universidad
     {
         public int id {get;set;}
+        [Required,StringLength(50)]
         public string universidad {get;set;}
-        //public ICollection<Facultad> Facultades { get; set; }
     }
 }
---
+++ SICAU/SICAU.App/SICAU.App.Frontend/Pages/DirEntidades/EditUniversidad.cshtml.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SICAU.App.Dominio;
+using SICAU.App.Persistencia;
+
+namespace SICAU.App.Frontend.Pages
+{
+    public class EditUniversidadModel : PageModel
+    {
+        private static IRepositorioUniversidad _repoUniversidad = new RepositorioUniversidad(new Persistencia.AppContext());
+        [BindProperty]
+        public Universidad universidad {get;set;}
+
+        public IActionResult OnGet(int? idUniversidad)
+        {
+            if (idUniversidad.HasValue)
+                universidad = _repoUniversidad.GetUniversidad(idUniversidad.Value);
+            else
+                universidad = new Universidad();
+            
+            if (universidad == null)
+                return RedirectToPage("./Personas");
+            else
+                return Page(); 
+        }
+
+        public IActionResult OnPost()
+        {
+            if(!ModelState.IsValid)
+                return Page();                
+            else
+            {
+                if(universidad.id > 0)
+                    universidad = _repoUniversidad.UpdateUniversidad(universidad);
+                else
+                {
+                    _repoUniversidad.AddUniversidad(universidad);
+                }     
+                return RedirectToPage("./ListUniversidad");
+            }     
+        }
+    }
+}
